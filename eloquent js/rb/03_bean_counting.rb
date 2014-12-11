779ec786e0faccbe49984a386fe7315d89586cd9
dividead <injuria.verbalis@gmail.com>
2014-12-11 09:20:56 +0000

    split

--- eloquent js/rb/03_bean_counting.rb
+++
@@ -1,5 +0,0 @@
-def countChars(x,l)
-  x.split(//).select{|i| i if i == l}.size #each_char
-end
-
-p countChars("kakkerlak", "k")
--- eloquent js/rb/03_minimum.rb
+++
@@ -1,5 +0,0 @@
-def min(x, y)
-  x < y ? x : y
-end
-p min(0,10)
-p min(0,-10)
