c70af70fc119bc7cd7070939cef18699c167e4d2
una owen <dividead@users.noreply.github.com>
2015-04-10 17:06:34 +0300

    Update fibonacci_iterator.js

--- es6/fibonacci_iterator.js
+++ es6/fibonacci_iterator.js
@@ -1,24 +1,24 @@
 function f(n){
-    function* fib(){
-        let n1 = 0
-        let n2 = 1
-        yield n1
-        yield n2
-        while(true){
-            let s = n2 + n1
-            n1 = n2
-            n2 = s
-            yield s
-        }
+  function* fib(){
+    let n1 = 0
+    let n2 = 1
+    yield n1
+    yield n2
+    while(true){
+        let s = n2 + n1
+        n1 = n2
+        n2 = s
+        yield s
     }
-    
-    let it = fib()
-    let a = []
-    
-    for(let i = 0; i < n; i++){
-        a.push(it.next().value)
-    }
-    return a
+  }
+  
+  let it = fib()
+  let a = []
+  
+  for(let i = 0; i < n; i++){
+    a.push(it.next().value)
+  }
+  return a
 }
 
 console.log(f(50))
