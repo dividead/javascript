52b36ecb1ea7ea243305584f6081e1f997c19942
dividead <dividead@users.noreply.github.com>
2015-08-19 23:20:55 +0300

    Update lispMap.js

--- lispMap.js
+++ lispMap.js
@@ -1,6 +1,4 @@
-function lispMap(f, ...a){
-  return a.map((m,i) => (a.map((n) => n[i]).reduce(f)))
-}
+let lispMap = (f, ...a) => a.map((m,i)=>(a.map((n) => n[i]).reduce(f)))
 
 lispMap((x,y)=>x+y, [1,2,3],[4,5,6],[7,8,9]) //[12,15,18]
 lispMap((x,y)=>x*y, [1,2,3],[4,5,6],[7,8,9]) //[28,80,162]
